c7a258fe5ac0f0a2bab94edfcdfde56cfcb61ab8
nadillustrator <nadin2208@gmail.com>
2022-12-20 20:41:45 +0300

    1-feature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Random;
+import java.util.Scanner;
 
+public class Main {
+    private final static Scanner scanner = new Scanner(System.in);
+    private final static Random random = new Random();
+    public static void main(String[] args) {
+        int guess = random.nextInt(10);
+        System.out.println("Enter number from 0 to 9: ");
+        int num = scanner.nextInt();
+        while (num != guess) {
+            System.out.println("Try one more time: ");
+            num = scanner.nextInt();
+        }
+            System.out.println("Congratulations! You guessed! Number " + guess);
     }
 }
